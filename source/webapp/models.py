86a4884e9105e09b58c71706273495864a1d968c
keny-stark <abdurakhmanov_i@iuca.kg>
2019-10-19 12:47:33 +0600

    сделал модель применил миграции и добавил данные зафикстурил

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    text = models.TextField(max_length=400, verbose_name='text of poll')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Time created')
+
+    def __str__(self):
+        return self.text
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey('webapp.Poll', related_name='choice',
+                             on_delete=models.CASCADE, verbose_name='poll')
+    text = models.TextField(max_length=400, verbose_name='text')
+
+    def __str__(self):
+        return self.text
+
